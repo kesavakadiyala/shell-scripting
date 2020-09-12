137d721e0b578c45364ae2efefce08204329411c
= <=>
2020-09-12 11:19:52 +0530

    Added respective path for output log

--- Project.sh
+++ Project.sh
@@ -35,7 +35,7 @@ Print(){
 case $1 in
   frontend)
     Print "Installing Nginx..."
-    yum install nginx -y >> output.log
+    yum install nginx -y >> /root/shell-scripting/output.log
     Status_Check
     Print "Downloading Frontend Application..."
     curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/db389ddc-b576-4fd9-be14-b373d943d6ee/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
@@ -43,7 +43,7 @@ case $1 in
     cd /usr/share/nginx/html
     rm -rf *
     Print "Extracting Frontend Application..."
-    unzip /tmp/frontend.zip >> output.log
+    unzip /tmp/frontend.zip >> /root/shell-scripting/output.log
     Status_Check
     mv static/* .
     rm -rf static README.md
