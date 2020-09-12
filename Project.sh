252b2f3bb8731151d5bacda2a3eadace142dc349
= <=>
2020-09-12 10:54:35 +0530

    Added Project

---
+++ Project.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+USER_ID = $(id -u)
+
+case $USER_ID in
+  0)
+    echo "Starting Installation"
+    ;;
+  *)
+    echo "You should be a root user for running $0 script."
+    exit
+    ;;
+esac
+
