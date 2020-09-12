f4fd10d5ee249629f22aff4775a7ee0de57f40a6
= <=>
2020-09-12 13:32:54 +0530

    Modified sed command

--- Project.sh
+++ Project.sh
@@ -68,7 +68,7 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
     Print "Installing Mongodb..."
     yum install -y mongodb-org >> output.log
     Status_Check
-    sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf
+    sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
     Status_Check
     Print "Downloading MongoDB Application..."
     curl -s -L -o /tmp/mongodb.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/e9218aed-a297-4945-9ddc-94156bd81427/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
