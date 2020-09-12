a27e121dabcf52be507c4b5bd14f34e455e79b99
= <=>
2020-09-12 12:54:49 +0530

    Modified MongoDB repo

--- Project.sh
+++ Project.sh
@@ -58,11 +58,11 @@ case $INPUT in
   mongodb)
     Print "Settingup MongoDB Repo..."
     echo '[mongodb-org-4.2]
-        name=MongoDB Repository
-        baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-        gpgcheck=1
-        enabled=1
-        gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
     Status_Check
     Print "Installing Mongodb..."
     yum install -y mongodb-org
