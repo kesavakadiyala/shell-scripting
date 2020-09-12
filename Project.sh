73a0b4d4f3b42708e7be6ed03ca48ee6c67e41eb
= <=>
2020-09-12 17:24:27 +0530

    Added Setup_Nodejs function and catalog installation.

--- Project.sh
+++ Project.sh
@@ -31,6 +31,43 @@ Print(){
   echo -e "\e[33m>> $1\e[0m"
 }
 
+Setup_Nojejs(){
+  Print "Installing Nodejs..."
+  yum install nodejs make gcc-c++ -y >> output.log
+  Status_Check
+  id roboshop
+  case $? in
+    1)
+      Print "Adding Application user..."
+      user add roboshop
+      Status_Check
+      ;;
+    *)
+  esac
+  Print "Downloading $1 Application..."
+  curl -s -L -o /tmp/$1.zip "$2"
+  Status_Check
+  cd /home/roboshop
+  mkdir catalogue
+  cd catalogue
+  Print "Extracting $1 Application..."
+  unzip -o /tmp/catalogue.zip >> output.log
+  Status_Check
+  Print "Installing Nodejs App Dependencies..."
+  npm --unsafe-perm install
+  Status_Check
+  chown roboshop:roboshop /home/roboshop -R
+  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+  Print "daemon-reloading..."
+  systemctl daemon-reload
+  Status_Check
+  Print "Starting $1 Application..."
+  systemctl enable catalogue >> output.log
+  systemctl start catalogue
+  Status_Check
+  Print "Done with $1 Application Installation..."
+}
+
 # Main Program
 
 case $INPUT in
@@ -57,7 +94,8 @@ case $INPUT in
     ;;
 
   mongodb)
-    Print "Settingup MongoDB Repo..."
+    Print "Setting 4u
+    p MongoDB Repo..."
     echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -88,6 +126,9 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
     mongo < users.js >> output.log
     Status_Check
     Print "Done with MongoDB Installation."
+    ;;
+  catalogue)
+    Setup_Nojejs "catalogue" "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/558568c8-174a-4076-af6c-51bf129e93bb/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
     ;;
   *)
     echo -e "\e[31mPlease mention proper input for $0 script. \nUsage: sh Project.sh frontend|mongodb|catalogue\e[0m"
