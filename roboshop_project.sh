f0cb5cce65ce3b9239538758471e128a842c1ba1
Kesava Kadiyala <kesavakadiyala@gmail.com>
2021-12-27 00:05:37 +0530

    Bug Fixes

--- roboshop_project.sh
+++ roboshop_project.sh
@@ -97,7 +97,7 @@ fi
 case $INPUT in
   frontend)
     Print "Installing Nginx..."
-    yum install nginx -y >> /root/shell-scripting/output.log
+    yum install nginx -y >> output.log
     Status_Check
     Print "Downloading Frontend Application..."
     curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/ce99914a-0f7d-4c46-9ccc-e4d025115ea9/_apis/git/repositories/db389ddc-b576-4fd9-be14-b373d943d6ee/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
@@ -105,7 +105,7 @@ case $INPUT in
     cd /usr/share/nginx/html
     rm -rf *
     Print "Extracting Frontend Application..."
-    unzip -o /tmp/frontend.zip >> /root/shell-scripting/output.log
+    unzip -o /tmp/frontend.zip >> output.log
     Status_Check
     mv static/* .
     rm -rf static README.md
